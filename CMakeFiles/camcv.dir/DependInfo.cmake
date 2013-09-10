8f1e1b5f2bb4621924c902f5f15e1bdbfe6778e3
Ning Cao <ncao@ualberta.ca>
2013-09-10 12:24:07 -0600

    Color object tracking for raspi

---
+++ CMakeFiles/camcv.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_C
+  "/home/pi/camcv/RaspiCLI.c" "/home/pi/camcv/CMakeFiles/camcv.dir/RaspiCLI.c.o"
+  "/home/pi/camcv/RaspiCamControl.c" "/home/pi/camcv/CMakeFiles/camcv.dir/RaspiCamControl.c.o"
+  "/home/pi/camcv/RaspiPreview.c" "/home/pi/camcv/CMakeFiles/camcv.dir/RaspiPreview.c.o"
+  "/home/pi/camcv/camcv_vid0.c" "/home/pi/camcv/CMakeFiles/camcv.dir/camcv_vid0.c.o"
+  )
+SET(CMAKE_C_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/include/opencv"
+  "/home/pi/opt/vc/host_applications/linux/libs/bcm_host/include"
+  "/home/pi/opt/vc/interface/vcos"
+  "/home/pi/opt/vc"
+  "/home/pi/opt/vc/interface/vcos/pthreads"
+  "/home/pi/opt/vc/interface/vmcs_host/linux"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
